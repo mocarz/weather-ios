131457f0a49b7214d64f5fd6031cc59dd2c6060c
Michal Mocarski <>
2024-09-25 14:37:23 +0200

    Created empty project

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Michal Mocarski on 25/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
